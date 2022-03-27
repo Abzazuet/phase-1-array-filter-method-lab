671f3bc24d3c6d0879ef3ba7ce4005805e0cc56e
Abel Zazueta <abel.zazueta@gmail.com>
2022-03-27 15:16:22 -0700

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const findMatching = function (array, string){
+    let selectedDrivers = array.filter(function(driver){
+        return driver.toLowerCase() == string.toLowerCase();
+    })
+    return selectedDrivers;
+}
+const fuzzyMatch = function(array, letters){
+    let selectedDrivers = array.filter(function(driver){
+        return driver.slice(0, letters.length)==letters;
+    })
+    return selectedDrivers;
+}
+const matchName = function(arrayOfObjects, string){
+    let selectedDrivers = arrayOfObjects.filter(function(driver){
+        return driver['name']==string;
+    })
+
+    return selectedDrivers;
+}
